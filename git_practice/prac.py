34e32b1648d0bc0f202048418e78b939ae6f92ab
tjdgus6255 <tjdgus6255@naver.com>
2021-01-21 21:03:03 +0900

    try again for practice

--- git_practice/prac.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-array1 = pd.Series([1, 2, 3], index=['A', 'B', 'C'])
-array2 = pd.Series([4, 5, 6], index=['B', 'C', 'D'])
-
-array = array1.add(array2, fill_value=0)
-print(array)
