9fe12319ac325e4917629c27b25e74b99099032c
tjdgus6255 <tjdgus6255@naver.com>
2021-02-21 12:02:27 +0900

    1

---
+++ programmers1.py
@@ -0,0 +1,12 @@
+def solution(mylist):
+    c = []
+    for i in range(len(mylist)):
+        c.append((len(mylist[i])))
+        answer = c
+    return answer
+#################################
+def solution(mylist):
+    return list(map(len, mylist))
+
+# 파이썬을 파이썬답게 [수강전 푸는 문제]
+# 내가 푼 코드(위), 답지(아래)
